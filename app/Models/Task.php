cc4e6ff6e8e312036e51d83f6af9dd4ecce0b466
Alain-Kay <kazadikayumba@gmail.com>
2024-02-28 15:24:16 +0100

    database shema

---
+++ app/Models/Task.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'task_id';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'due_date',
+        'status',
+        'contact_id',
+        'company_id',
+        'opportunity_id',
+    ];
+
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function opportunity()
+    {
+        return $this->belongsTo(Opportunity::class);
+    }
+}
