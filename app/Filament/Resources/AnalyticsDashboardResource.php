24b346bf61a2aeae391761dc7e19ccc79c03541b
Curtis Delicata <curtis.delicata@outlook.com>
2024-09-29 22:51:38 +0100

    Enhance Reporting and Analytics Capabilities

---
+++ app/Filament/Resources/AnalyticsDashboardResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AnalyticsDashboardResource\Pages;
+use App\Models\Contact;
+use App\Models\Deal;
+use Filament\Forms;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use Filament\Widgets\ChartWidget;
+
+class AnalyticsDashboardResource extends Resource
+{
+    protected static ?string $model = Contact::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-presentation-chart-line';
+
+    protected static ?string $navigationLabel = 'Analytics Dashboard';
+
+    public static function getWidgets(): array
+    {
+        return [
+            Widgets\ContactStatsOverview::class,
+            Widgets\SalesPipelineChart::class,
+            Widgets\CustomerEngagementChart::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ViewAnalyticsDashboard::route('/'),
+        ];
+    }
+}
