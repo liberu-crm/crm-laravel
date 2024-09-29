5590e9a82952b5e443ffdc700d9a63bb938018f1
Curtis Delicata <curtis.delicata@outlook.com>
2024-09-29 22:26:33 +0100

    Implement WhatsApp Integration and Ticket Creati

---
+++ app/Services/WhatsAppBusinessService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class WhatsAppBusinessService
+{
+    protected $apiUrl;
+    protected $accessToken;
+
+    public function __construct()
+    {
+        $this->apiUrl = config('services.whatsapp.api_url');
+        $this->accessToken = config('services.whatsapp.access_token');
+    }
+
+    public function getUnreadMessages()
+    {
+        $response = Http::withToken($this->accessToken)
+            ->get($this->apiUrl . '/messages', [
+                'status' => 'unread',
+                'limit' => 10,
+            ]);
+
+        return $response->json()['messages'] ?? [];
+    }
+
+    public function getMessage($messageId)
+    {
+        $response = Http::withToken($this->accessToken)
+            ->get($this->apiUrl . '/messages/' . $messageId);
+
+        return $response->json();
+    }
+
+    public function sendReply($to, $body)
+    {
+        $response = Http::withToken($this->accessToken)
+            ->post($this->apiUrl . '/messages', [
+                'to' => $to,
+                'type' => 'text',
+                'text' => [
+                    'body' => $body
+                ]
+            ]);
+
+        return $response->json();
+    }
+}
