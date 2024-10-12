6b04819bbb77c7f6030b2f1e95128bf623ccc676
Curtis Delicata <curtis.delicata@outlook.com>
2024-10-12 12:22:00 +0000

    Update namespace

--- app/Filament/App/Resources/LandingPageResource.php
+++ app/Filament/App/Resources/LandingPageResource.php
@@ -5,9 +5,9 @@ namespace App\Filament\App\Resources;
 use App\Filament\App\Resources\LandingPageResource\Pages;
 use App\Models\LandingPage;
 use Filament\Forms;
-use Filament\App\Resources\Form;
+use Filament\Forms\Form;
 use Filament\App\Resources\Resource;
-use Filament\App\Resources\Table;
+use Filament\Tables\Table;
 use Filament\Tables;
 
 class LandingPageResource extends Resource
