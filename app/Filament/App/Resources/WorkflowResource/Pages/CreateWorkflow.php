99d4d3f21702636fef50018b3667b9166240f6cf
Curtis Delicata <curtis.delicata@outlook.com>
2024-10-16 22:19:35 +0000

    Fixes

---
+++ app/Filament/App/Resources/WorkflowResource/Pages/CreateWorkflow.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\App\Resources\WorkflowResource\Pages;
+
+use App\Filament\App\Resources\WorkflowResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateWorkflow extends CreateRecord
+{
+    protected static string $resource = WorkflowResource::class;
+}
