e2949878de0003ab65b16ca3efe4ea37b40f25df
Curtis Delicata <curtis.delicata@outlook.com>
2024-12-13 12:07:37 +0000

    Update TeamSubscriptionResource.php

--- app/Filament/Resources/TeamSubscriptionResource.php
+++ app/Filament/Resources/TeamSubscriptionResource.php
@@ -5,9 +5,9 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\TeamSubscriptionResource\Pages;
 use App\Models\TeamSubscription;
 use Filament\Forms;
-use Filament\Resources\Form;
+use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Resources\Table;
+use Filament\Tables\Table;
 use Filament\Tables;
 
 class TeamSubscriptionResource extends Resource
