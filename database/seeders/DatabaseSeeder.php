d23e7ba335771115ff9aa54ddeef454bfab41eaf
Curtis Delicata <curtis.delicata@outlook.com>
2025-09-30 14:16:34 +0000

    Update dependencies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SiteSettingsSeeder::class,
+//            SiteSettingsSeeder::class,
             MenuSeeder::class,
             TeamSeeder::class,
             RolesSeeder::class,
