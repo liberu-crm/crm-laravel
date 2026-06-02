a8d4026761e2db597d201057c814155a5dfa0eff
Azhar Uddin <azhar.cu25@gmail.com>
2026-06-03 03:13:01 +0600

    fix: personal access tokens

---
+++ resources/views/filament/pages/profile/personal-access-tokens.blade.php
@@ -0,0 +1,5 @@
+<x-filament-panels::page>
+    <x-filament::section>
+        @livewire(\Laravel\Jetstream\Http\Livewire\ApiTokenManager::class)
+    </x-filament::section>
+</x-filament-panels::page>
